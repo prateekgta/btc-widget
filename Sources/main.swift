a095e8bef0d8fd95ee378fe5987b8c5addda5cb9
PrateekAir <prateekair@PrateekAirs-Laptop.local>
2026-04-06 00:48:16 -0700

    Auto-terminate process when window closes

--- Sources/main.swift
+++ Sources/main.swift
@@ -3,4 +3,8 @@ import AppKit
 let app = NSApplication.shared
 let delegate = AppDelegate()
 app.delegate = delegate
+app.setActivationPolicy(.regular)
+app.activate(ignoringOtherApps: true)
 app.run()
+
+exit(0)
